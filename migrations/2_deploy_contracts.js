9d94e1886cb505e9d149f5a01cb783b18f001e5d
Nikita Fedorec <fedorecn98@yandex.ru>
2018-06-25 22:49:18 +0300

    Remove old commented-out code from a migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,9 +2,5 @@
 var SotaToken = artifacts.require("./SotaToken.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(SafeMath);
-  // deployer.deploy(ERC20);
-  // deployer.link(SafeMath, SotaToken);
-  // deployer.link(ERC20, SotaToken);
   deployer.deploy(SotaToken);
 };
